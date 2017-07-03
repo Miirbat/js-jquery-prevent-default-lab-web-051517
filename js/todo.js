ec3dc98866aafddc6dfa18304b5c90b5d87d0479
github username <github email address>
2017-07-03 13:37:46 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm()
 });
 
-// define functions here
+
+function submitForm(){
+$('form').submit(function(event){
+  var listItem = $('#item').val() //is value of id item, which is a filled out form.... via HTML form tag...
+  $('ol').append( '<li>' + listItem + '</li>' ) //always appending strings and shit. jquery takes things in strings it seems.
+  event.preventDefault();
+})
+}
